d5f47213f293fcb30aff6ac60d02dad407a2e3cd
mdmrts <mervedemirtas.md@gmail.com>
2021-11-11 09:36:44 -0500

    Task:updated some configurations

--- calc/division.py
+++ calc/division.py
@@ -1,13 +1,12 @@
-        """This is the division calculation. Values A and B come from the Calculation class"""
-        from calc.calculation import Calculation
+"""This is the division calculation. Values A and B come from the Calculation class"""
+from calc.calculation import Calculation
 
-        class Division(Calculation):
-            """The Division class has one method, to get the result of the calculation.
-            A and B come from the Calculation parent Class"""
+class Division(Calculation):
+    """The Division class has one method, to get the result of the calculation.
+    A and B come from the Calculation parent Class"""
 
-            def get_result(self):
-                """This does the actual division. Displays an error if dividing by 0"""
-                if self.value_b == 0:
-                    return "Error, cannot divide by zero."
-                return self.value_a / self.value_b
-
+    def get_result(self):
+        """This does the actual division. Displays an error if dividing by 0"""
+        if self.value_b == 0:
+            return "Error, cannot divide by zero."
+        return self.value_a / self.value_b
