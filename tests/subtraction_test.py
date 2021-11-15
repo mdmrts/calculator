947b6c826861a874b4e92e8e67b823e16aac5be1
mdmrts <mervedemirtas.md@gmail.com>
2021-11-15 16:52:42 -0500

    Task:changes a few numbers for calculate

--- tests/subtraction_test.py
+++ tests/subtraction_test.py
@@ -4,8 +4,8 @@ from calc.calculations.subtraction import Subtraction
 def test_calculation_subtraction():
     """testing that our calculator has a static method for subtraction"""
     #Arrange
-    mynumbers = (20.0,4.0,3.0)
+    mynumbers = (10.0,4.0,3.0)
     subtraction = Subtraction(mynumbers)
     #Act
     #Assert
-    assert subtraction.get_result() == -27.0
+    assert subtraction.get_result() == -17.0
